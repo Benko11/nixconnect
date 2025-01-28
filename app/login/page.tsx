133dffe43a0761adaa2f857008b6a4fe7c1acf84
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2025-01-28 13:14:06 +0100

    fix: add <Login /> suspense

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Login from "../ui/forms/login";
 
 export default function Page() {
@@ -7,7 +8,9 @@ export default function Page() {
         <h1 className="text-xl pb-2">Log In</h1>
 
         <div className="bg-default-neutral p-4">
-          <Login />
+          <Suspense>
+            <Login />
+          </Suspense>
         </div>
       </div>
     </div>
