d84a3469e314df8752a6b019285a156231fa8120
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2025-01-29 07:34:33 +0100

    refactor: Prisma implementation

--- app/db/setup/seeds/route.ts
+++ app/db/setup/seeds/route.ts
@@ -1,46 +1,100 @@
 import { createClient } from "@vercel/postgres";
+import { PrismaClient } from "@prisma/client";
 
 const client = createClient();
 await client.connect();
 
+const prisma = new PrismaClient();
+
 export async function GET() {
   try {
-    await client.sql`BEGIN`;
     await seedGenders();
     await seedColourSchemes();
     await seedPronouns();
-    await client.sql`COMMIT`;
-    await client.end();
 
     return Response.json({ message: "Data seeded successfully" });
   } catch (error) {
-    await client.sql`ROLLBACK`;
-    await client.end();
-
     return Response.json({ error }, { status: 500 });
   }
 }
 
 async function seedGenders() {
-  await client.sql`INSERT INTO genders(name, description) VALUES
-    ('Male', 'Men or men-presenting individuals'),
-    ('Female', 'Women or women-presenting individuals'),
-    ('Non-binary', 'Being on the spectrum between male and female or being outside it');`;
+  const newGenders = await prisma.gender.createMany({
+    data: [
+      {
+        name: "Male",
+        description: "Men or men-presenting individuals",
+      },
+      {
+        name: "Female",
+        description: "Women or women-presenting individuals",
+      },
+      {
+        name: "Non-binary",
+        description:
+          "Being on the spectrum between male and female or being outside it",
+      },
+    ],
+    skipDuplicates: true,
+  });
+
+  console.log(newGenders);
 }
 
 async function seedColourSchemes() {
-  await client.sql`INSERT INTO colour_schemes(name,primary_colour,secondary_colour,accent_colour,neutral_colour,error_colour,dark_colour,light_colour,background_colour) VALUES
-  ('Default', 'hsl(220, 70%, 70%)', 'hsl(12, 80%, 27%)', 'hsl(300, 80%, 30%)', 'hsl(0, 0%, 20%)', 'hsl(60, 100%, 50%)', 'hsl(220, 5%, 10%)', 'hsl(220, 5%, 90%)', '#0B0C0E'),
-  ('Synthwave', '#C724B1', '#642F6C', '#58A7AF', 'hsl(300, 5%, 20%)', '#71DBD4', '#3A3A59', '#F3E9F4', '#0b0c0e'),
-  ('Alpine', '#75eef0', '#005E57', '#203F21', '#22242a', '#668EAB', '#2c352c', '#ccfeff', '#0b0c0e2')`;
+  const newColourSchemes = await prisma.colourScheme.createMany({
+    data: [
+      {
+        name: "Default",
+        primaryColour: "hsl(220, 70%, 70%)",
+        secondaryColour: "hsl(12, 80%, 27%)",
+        accentColour: "hsl(300, 80%, 30%)",
+        errorColour: "hsl(60, 100%, 50%)",
+        neutralColour: "hsl(0, 0%, 20%)",
+        lightColour: "hsl(220, 5%, 90%)",
+        darkColour: "hsl(220, 5%, 10%)",
+        backgroundColour: "#0B0C0E",
+      },
+      {
+        name: "Synthwave",
+        primaryColour: "#C724B1",
+        secondaryColour: "#642F6C",
+        accentColour: "#58A7AF",
+        errorColour: "#71DBD4",
+        neutralColour: "hsl(300, 5%, 20%)",
+        lightColour: "#F3E9F4",
+        darkColour: "#3A3A59",
+        backgroundColour: "#0B0C0E",
+      },
+      {
+        name: "Alpine",
+        primaryColour: "#75eef0",
+        secondaryColour: "#005E57",
+        accentColour: "#203F21",
+        errorColour: "#668EAB",
+        neutralColour: "22242a",
+        lightColour: "#ccfeff",
+        darkColour: "#2c352c",
+        backgroundColour: "#0B0C0E",
+      },
+    ],
+    skipDuplicates: true,
+  });
+
+  console.log(newColourSchemes);
 }
 
 async function seedPronouns() {
-  await client.sql`INSERT INTO pronouns(word,master_pronoun) VALUES
-  ('he', NULL),
-  ('him', 1),
-  ('she', NULL),
-  ('her', 3),
-  ('they', NULL),
-  ('them', 5)`;
+  const newPronouns = await prisma.pronoun.createMany({
+    data: [
+      { word: "he" },
+      { word: "him", masterPronounId: 1 },
+      { word: "she" },
+      { word: "her", masterPronounId: 3 },
+      { word: "they" },
+      { word: "them", masterPronounId: 5 },
+    ],
+    skipDuplicates: true,
+  });
+  console.log(newPronouns);
 }
