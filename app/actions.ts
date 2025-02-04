11498db5374ef7e074fb5a64722d20263f07b70a
Benjamin Bergstrom <benko.dubovecky@gmail.com>
2025-02-04 18:29:36 +0100

    fix: build error

--- app/actions.ts
+++ app/actions.ts
@@ -263,3 +263,43 @@ export async function createAccount(formData: FormData) {
 
   console.log("totally adding the user...");
 }
+
+export const getAllPronouns = async (): Promise<string[][]> => {
+  const supabase = await createClient();
+
+  const { data: masterPronounsData, error: masterError } = await supabase
+    .from("pronouns")
+    .select("id, word")
+    .is("master_pronoun_id", null)
+    .order("id");
+
+  if (masterError) {
+    console.error("Error fetching master pronouns:", masterError);
+    return [];
+  }
+
+  if (!masterPronounsData || masterPronounsData.length === 0) {
+    return [];
+  }
+
+  const allPronouns: string[][] = await Promise.all(
+    masterPronounsData.map(async (masterPronoun) => {
+      const { data: subPronounsData, error: subError } = await supabase
+        .from("pronouns")
+        .select("id,word")
+        .eq("master_pronoun_id", masterPronoun.id);
+
+      if (subError) {
+        console.error(
+          `Error fetching sub-pronouns for master_pronoun_id ${masterPronoun.id}:`,
+          subError
+        );
+        return [masterPronoun.word];
+      }
+
+      return [masterPronoun, ...subPronounsData];
+    })
+  );
+
+  return allPronouns;
+};
--- app/api/pronouns/route.tsx
+++ app/api/pronouns/route.tsx
@@ -1,46 +1,6 @@
-import { createClient } from "@/utils/supabase/server";
+import { getAllPronouns } from "@/app/actions";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   return NextResponse.json(await getAllPronouns());
 }
-
-export const getAllPronouns = async (): Promise<string[][]> => {
-  const supabase = await createClient();
-
-  const { data: masterPronounsData, error: masterError } = await supabase
-    .from("pronouns")
-    .select("id, word")
-    .is("master_pronoun_id", null)
-    .order("id");
-
-  if (masterError) {
-    console.error("Error fetching master pronouns:", masterError);
-    return [];
-  }
-
-  if (!masterPronounsData || masterPronounsData.length === 0) {
-    return [];
-  }
-
-  const allPronouns: string[][] = await Promise.all(
-    masterPronounsData.map(async (masterPronoun) => {
-      const { data: subPronounsData, error: subError } = await supabase
-        .from("pronouns")
-        .select("id,word")
-        .eq("master_pronoun_id", masterPronoun.id);
-
-      if (subError) {
-        console.error(
-          `Error fetching sub-pronouns for master_pronoun_id ${masterPronoun.id}:`,
-          subError
-        );
-        return [masterPronoun.word];
-      }
-
-      return [masterPronoun, ...subPronounsData];
-    })
-  );
-
-  return allPronouns;
-};
